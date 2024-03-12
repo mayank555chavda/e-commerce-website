6078b08c8af60a45c076a4393206871dfea4b4b2
mayank555chavda <155214844+mayank555chavda@users.noreply.github.com>
2024-03-12 10:06:14 +0530

    Add files via upload

---
+++ phone grips.js
@@ -0,0 +1,83 @@
+const originalImage1 = document.getElementById('myImage1');
+
+originalImage1.onmouseover = function() {
+  originalImage1.src = 'image/c1-1.jpg'; // Change 'new-image.jpg' to the path of your new image
+}
+
+originalImage1.onmouseout = function() {
+  originalImage1.src = 'image/c1.jpg'; // Change 'original-image.jpg' to the path of your original image
+}
+
+
+
+const originalImage2 = document.getElementById('myImage2');
+
+originalImage2.onmouseover = function() {
+  originalImage2.src = 'image/c2-2.jpg'; // Change 'new-image.jpg' to the path of your new image
+}
+
+originalImage2.onmouseout = function() {
+  originalImage2.src = 'image/c2.jpg'; // Change 'original-image.jpg' to the path of your original image
+}
+
+
+const originalImage3 = document.getElementById('myImage3');
+
+originalImage3.onmouseover = function() {
+  originalImage3.src = 'image/c3-3.jpg'; // Change 'new-image.jpg' to the path of your new image
+}
+
+originalImage3.onmouseout = function() {
+  originalImage3.src = 'image/c3.jpg'; // Change 'original-image.jpg' to the path of your original image
+}
+
+const originalImage4 = document.getElementById('myImage4');
+
+originalImage4.onmouseover = function() {
+  originalImage4.src = 'image/c4-4.jpg'; // Change 'new-image.jpg' to the path of your new image
+}
+
+originalImage4.onmouseout = function() {
+  originalImage4.src = 'image/c4.jpg'; // Change 'original-image.jpg' to the path of your original image
+}
+
+const originalImage5 = document.getElementById('myImage5');
+
+originalImage5.onmouseover = function() {
+  originalImage5.src = 'image/c5-5.jpg'; // Change 'new-image.jpg' to the path of your new image
+}
+
+originalImage5.onmouseout = function() {
+  originalImage5.src = 'image/c5.jpg'; // Change 'original-image.jpg' to the path of your original image
+}
+
+
+const originalImage6 = document.getElementById('myImage6');
+
+originalImage6.onmouseover = function() {
+  originalImage6.src = 'image/c6-6.jpg'; // Change 'new-image.jpg' to the path of your new image
+}
+
+originalImage6.onmouseout = function() {
+  originalImage6.src = 'image/c6.jpg'; // Change 'original-image.jpg' to the path of your original image
+}
+
+const originalImage7 = document.getElementById('myImage7');
+
+originalImage7.onmouseover = function() {
+  originalImage7.src = 'image/c7-7.jpg'; // Change 'new-image.jpg' to the path of your new image
+}
+
+originalImage7.onmouseout = function() {
+  originalImage7.src = 'image/c7.jpg'; // Change 'original-image.jpg' to the path of your original image
+}
+
+const originalImage8 = document.getElementById('myImage8');
+
+originalImage8.onmouseover = function() {
+  originalImage8.src = 'image/c8-8.jpg'; // Change 'new-image.jpg' to the path of your new image
+}
+
+originalImage8.onmouseout = function() {
+  originalImage8.src = 'image/c8.jpg'; // Change 'original-image.jpg' to the path of your original image
+}
